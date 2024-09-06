aa1f345b64df432e9dcf1be5b61eb8712ba9d723
PG Kim <vakon1@daum.net>
2024-09-06 12:23:13 +0900

    중복 코드 제거, 불편한 부분 고치기

--- script.js
+++ script.js
@@ -22,19 +22,11 @@ function registerBook() {
     if (isExist) { // 존재하면 추가 거부
       alert('이미 등록된 도서입니다.');
     } else {
-      bookList.push({ BOOK_NO: BOOK_NO, category: bookCategory, name: bookName, price: parseFloat(bookPrice) });
-      let row = `
-        <tr>
-          <td>${BOOK_NO++}</td>
-          <td>${bookCategory}</td>
-          <td>${bookName}</td>  
-          <td>${bookPrice}</td>
-          <td><button onclick="removeBook(${bookList.length - 1})">삭제</button></td>
-        </tr>
-      `;
-      tableBody.insertAdjacentHTML("beforeend", row);
+      const newBook = { BOOK_NO: BOOK_NO++, category: bookCategory, name: bookName, price: bookPrice };
+      bookList.push(newBook);
+      addBook(newBook);
     }
-  } else {
+  } else { // 빈칸 체크
     let errors = [];
     if (bookCategory === "") errors.push("카테고리가 선택되지 않았습니다.");
     if (bookName === "") errors.push("책 이름이 빈칸입니다.");
@@ -44,47 +36,45 @@ function registerBook() {
   }
 }
 
+// 코드가 복잡해서 함수로 뺌
+function addBook(book) {
+  const row = `
+  <tr>
+    <td>${book.BOOK_NO}</td>
+    <td>${book.category}</td>
+    <td>${book.name}</td>  
+    <td>${book.price}</td>
+    <td><button onclick="removeBook(${book.BOOK_NO})">삭제</button></td>
+  </tr>
+`;
+  tableBody.insertAdjacentHTML("beforeend", row);
+}
+
 // 책 목록 출력기능
 function printBookList() {
-
   tableBody.innerHTML = '';
-  bookList.forEach((book) => {
-    let row = `
-      <tr>
-        <td>${book.BOOK_NO}</td>
-        <td>${book.category}</td>
-        <td>${book.name}</td>  
-        <td>${book.price}</td>
-        <td><button onclick="removeBook(${book.BOOK_NO})">삭제</button></td>
-      </tr>
-    `;
-    tableBody.insertAdjacentHTML("beforeend", row);
-  });
-
+  bookList.forEach(el => addBook(el));
 }
 
 // 책 삭제 기능
-function removeBook(index) {
-  console.log("들어온 BOOK_NO:", index);
-  console.log(bookList);
-  for (let i = 0; i < bookList.length; i++) {
-    if (index === bookList[i].BOOK_NO) {
-      console.log("찾은 BOOK_NO", bookList[i].BOOK_NO);
-      bookList.splice(i, 1);
-    }
+// findIndex(): 배열에서 특정 조건을 만족하는 요소를 찾아 첫 번째 요소의 인덱스를 반환하는 함수
+function removeBook(bookNo) {
+  const index = bookList.findIndex(book => book.BOOK_NO === bookNo);
+  if (index !== -1) {
+    bookList.splice(index, 1);
+    printBookList();
   }
-  printBookList();
 }
 
 // 책 목록 정렬
 function sortBookList() {
+  const sortType = sortSelect.value;
 
-
-  if (sortSelect.value === 'ascending') { // 오름차순
+  if (sortType === 'ascending') { // 오름차순
     bookList.sort((a, b) => a.price - b.price);
-  } else if (sortSelect.value === 'descending') { // 내림차순
+  } else if (sortType === 'descending') { // 내림차순
     bookList.sort((a, b) => b.price - a.price);
-  } else if (sortSelect.value === 'bookno') { // 도서번호순
+  } else if (sortType === 'bookno') { // 도서번호순
     bookList.sort((a, b) => a.BOOK_NO - b.BOOK_NO)
   }
   printBookList();
@@ -93,10 +83,9 @@ function sortBookList() {
 
 // 책 검색
 function searchBooks() {
-
   const syntax = inputSearch.value.toLowerCase();
 
-  if (syntax === '') {
+  if (!syntax) {
     alert('검색 칸이 비어있습니다.\n검색어를 입력해주세요.');
   } else {
     const filter = bookList.filter(book =>
@@ -104,42 +93,23 @@ function searchBooks() {
       book.category.toLowerCase().includes(syntax));
 
     tableBody.innerHTML = '';
-    filter.forEach((book) => {
-      let row = `
-      <tr>
-        <td>${book.BOOK_NO}</td>
-        <td>${book.category}</td>
-        <td>${book.name}</td>  
-        <td>${book.price}</td>
-        <td><button onclick="removeBook(${bookList.indexOf(book.BOOK_NO)})">삭제</button></td>
-      </tr>
-    `;
-      tableBody.insertAdjacentHTML("beforeend", row);
-    });
+    filter.forEach(el => addBook(el));
   }
 }
 
-// 엔터키 입력으로 추가하기
-function bookEnterKey(event) {
-  if (event.key === 'Enter') {
-    event.preventDefault(); // 없으면 이상해짐
-    registerBook();
-  }
-}
-
-// 엔터키 입력으로 검색하기
-function searchEnterKey(event) {
-  if (event.key === 'Enter') {
-    event.preventDefault(); // 없으면 이상해짐
-    searchBooks();
+// 엔터키로 입력받게
+function enterKey(key, func) {
+  if (key.key === 'Enter') {
+    key.preventDefault();
+    func();
   }
 }
 
 // 이벤트 리스너 추가
 inputSearchBtn.addEventListener('click', searchBooks);
 sortSelect.addEventListener('change', sortBookList);
-bookPriceInput.addEventListener('keypress', bookEnterKey);  // 엔터키
-searchInput.addEventListener('keypress', searchEnterKey);  // 엔터키
+bookPriceInput.addEventListener('keypress', key => enterKey(key, registerBook));
+searchInput.addEventListener('keypress', key => enterKey(key, searchBooks));
 
 printBookList();
 
@@ -161,11 +131,12 @@ bookList = [
   { BOOK_NO: 12, category: '시/에세이', name: 'Book2', price: 16000 },
   { BOOK_NO: 13, category: '시/에세이', name: 'lowercase_book1', price: 15900 },
   { BOOK_NO: 14, category: '시/에세이', name: 'lowercase_book2', price: 18000 },
+  { BOOK_NO: 15, category: '시/에세이', name: 'UPPERCASE_BOOK1', price: 23800 },
+  { BOOK_NO: 16, category: '시/에세이', name: 'UPPERCASE_BOOK2', price: 33800 }
 ];
 
 // BOOK_NO 재설정 확인
 BOOK_NO = bookList.length + 1;
-console.log(BOOK_NO);
 
 // 도서 목록 출력
 printBookList();
